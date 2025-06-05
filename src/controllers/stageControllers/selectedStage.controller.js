f892eb8e29bd1bc6c7af92b6a13c931bcdf7ec87
Digisangha <digisangha@gmail.com>
2025-06-05 10:12:45 +0530

    updt: show if previous submitted response stage under selected stage api

--- src/controllers/stageControllers/selectedStage.controller.js
+++ src/controllers/stageControllers/selectedStage.controller.js
@@ -1,15 +1,38 @@
 import { Stage } from "../../models/stage.model.js";
 import { ApiResponse } from "../../utils/helper/ApiResponse.js";
 import { asyncHandler } from "../../utils/helper/AsyncHandler.js";
+import { UserStageProgress } from "../../models/userStageProgress.model.js";
 
 const selectedStage = asyncHandler(async (req, res) => {
-  const stage = await Stage.findById(req.params.stageId).lean();
+  const { _id: userId } = req.user;
+  const stageId = req.params.stageId;
+
+  const stage = await Stage.findById(stageId).lean();
   if (!stage) {
     return res.status(404).json(new ApiResponse(404, {}, "Stage not found"));
   }
-  return res
-    .status(200)
-    .json(new ApiResponse(200, { ...stage }, "Selected stage data."));
+
+  const checkUserPrevStageResponse = await UserStageProgress.findOne({
+    userId,
+    stageId,
+  })
+    .sort({ createdAt: -1 }) // Only matters if multiple responses are allowed
+    .lean();
+
+  const isPrevSubmitted = !!checkUserPrevStageResponse;
+  const previousStatus = checkUserPrevStageResponse?.status || "";
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        ...stage,
+        isPrevSubmitted,
+        previousStatus,
+      },
+      "Selected stage data."
+    )
+  );
 });
 
 export { selectedStage };
